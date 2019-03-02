dc06d0d6e1131347b3f4c8eb1fb1097067847b0a
alparslandev <alp.develioglu@gmail.com>
2019-03-03 02:01:37 +0300

    LoginActivity Signin button enability fixed

--- app/src/main/java/com/alp/usermanager/LoginActivity.kt
+++ app/src/main/java/com/alp/usermanager/LoginActivity.kt
@@ -2,11 +2,49 @@ package com.alp.usermanager
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import com.alp.usermanager.wrappers.TextWatcherWrapper
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.button_primary.*
 
-class LoginActivity : AppCompatActivity() {
+abstract class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        btn_primary.isEnabled = false
+
+        et_mail.addTextChangedListener(object : TextWatcherWrapper() {
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                checkButtonAvailability()
+            }
+        })
+
+        et_password.addTextChangedListener(object : TextWatcherWrapper() {
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                checkButtonAvailability()
+            }
+        })
+
+        // todo setOnClickListener will be added
+
+    }
+
+    fun checkButtonAvailability() {
+        var isEnabled = getMailConditions() && getPasswordConditions()
+
+        btn_primary.isEnabled = isEnabled
+    }
+
+    private fun getMailConditions(): Boolean {
+        val text = et_mail.text
+        return !TextUtils.isEmpty(text) && text.endsWith(".com")
+    }
+
+    private fun getPasswordConditions(): Boolean {
+        val text = et_password.text
+        return !TextUtils.isEmpty(text) && text.length > 4
     }
 }
---
+++ app/src/main/java/com/alp/usermanager/wrappers/TextWatcherWrapper.kt
@@ -0,0 +1,15 @@
+package com.alp.usermanager.wrappers
+
+import android.text.Editable
+import android.text.TextWatcher
+
+open class TextWatcherWrapper : TextWatcher {
+    override fun afterTextChanged(p0: Editable?) {
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+}
